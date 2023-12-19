c1d52ea201f0cdc6a610df5c0dd5e9308da9b3fe
Ugenteraan Manogaran <m.ugenteraan15@gmail.com>
2023-12-19 13:47:41 +0800

    latest;

---
+++ models/multiblock.py
@@ -0,0 +1,13 @@
+'''Mask Collator module that calculates the mask.
+'''
+
+
+
+
+class MultiBlockMaskCollator:
+    '''This module will be supplied in the collate parameter during the initialization of dataloader.
+    '''
+
+
+    def __init__(selfi, image_height=224, image_width=224, patch_size=14, ):
+        pass
