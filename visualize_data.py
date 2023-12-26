c554cf87ebad98e6c3c23c36bca06c135da828f0
Ugenteraan Manogaran <m.ugenteraan15@gmail.com>
2023-12-26 13:51:03 +0800

    progress data visualizationg

--- visualize_data.py
+++ visualize_data.py
@@ -5,25 +5,19 @@
 from load_dataset import DeepLakeDataset
 import matplotlib.pyplot as plt
 
-import cred
 
 
 
 class VisualizeData:
 
-    def __init__(self, deeplake_module, deeplake_dataset, visualize_batch_size, visualize_shuffle=True):
-        
-        self.deeplake_module = deeplake_module
-        self.visualize_batch_size = visualize_batch_size
-        self.visualize_shuffle = visualize_shuffle
+    def __init__(self, deeplake_module, deeplake_dataset, visualize_batch_size, visualize_shuffle, deeplake_token):
 
-
-        
-        
-        
+        self.dataloader = deeplake_module(token=deeplake_token, deeplake_dataset=deeplake_dataset, batch_size=visualize_batch_size, shuffle=visualize_shuffle)
 
     def __call__(self):
+
         
+         
 
 
 m = DeepLakeDataset(token=cred.DEEPLAKE_TOKEN, deeplake_dataset='hub://activeloop/imagenet-train', batch_size=2, shuffle=False)()
