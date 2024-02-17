8425cd9d7e2fa2e6469492ba748c6a7f4de89e77
Ugenteraan <m.ugenteraan15@gmail.com>
2024-02-17 23:59:23 +0800

    debug both networks

--- models/vit.py
+++ models/vit.py
@@ -17,7 +17,7 @@ from utils import apply_masks_over_embedded_patches
 
 class VisionTransformerForPredictor(nn.Module):
 
-    def __init__(self, input_dim, num_patches, predictor_network_embedding_dim, device, **kwargs):
+    def __init__(self, input_dim, num_patches, predictor_network_embedding_dim, device, num_class, classification_embedding_dim, **kwargs):
         '''Vision Transformer to be used as the predictor. The output from the predictor will be in the same dimension as the input since the output is trying to predict the embedding of the target images.
         '''
 
@@ -39,17 +39,23 @@ class VisionTransformerForPredictor(nn.Module):
         #apply layernorm on the output of the transformer blocks.
         self.final_layernorm = nn.LayerNorm(input_dim).to(device)
 
-        self.predictor_projector = nn.Linear(self.predictor_network_embedding_dim, input_dim).to(device) 
+        # self.predictor_projector = nn.Linear(self.predictor_network_embedding_dim, input_dim).to(device) 
+
+        self.classification_head = nn.Sequential(einops_torch.Reduce('b n e -> b e', reduction='mean'),
+                                                 nn.LayerNorm(self.predictor_network_embedding_dim),
+                                                 nn.Linear(self.predictor_network_embedding_dim, classification_embedding_dim),
+                                                 nn.GELU(),
+                                                 nn.Linear(classification_embedding_dim, num_class)).to(self.device)
 
 
 
-    def forward(self, x, masks_ctxt, masks_pred_target):
+    def forward(self, x, masks_ctxt=None, masks_pred_target=None):
 
         x = self.predictor_embed(x)
 
-        batch_size = len(x) // len(masks_ctxt) #this has to be done because the  apply_masks_over_embedded_patches function from utils.py causes the input tensor to be expanded by the number of masks given. In other words, the batch dimension value will be (original batch num x number of masks). Therefore, to get the right batch number, we do the division. REMEMBER, the function mentioned before is used with the context mask before the output comes to the predictor.
+        batch_size = x.size(0) #this has to be done because the  apply_masks_over_embedded_patches function from utils.py causes the input tensor to be expanded by the number of masks given. In other words, the batch dimension value will be (original batch num x number of masks). Therefore, to get the right batch number, we do the division. REMEMBER, the function mentioned before is used with the context mask before the output comes to the predictor.
         
-        _, num_ctxt, _ = x.size() #to get the size of the context mask.
+        # _, num_ctxt, _ = x.size() #to get the size of the context mask.
 
         #generate the positional embedding tokens using the original image embedding sizes. Not the tensor after any mask(s) applied. 
         #the positional embeddings have to be masked with both the masks (pred/target masks and context masks) separately.
@@ -63,28 +69,28 @@ class VisionTransformerForPredictor(nn.Module):
         pos_embedding_ctxt = pos_embedding_pred_target.clone().detach() #for the context mask
 
         
-        #apply the masks on the appropriate positional embedding. 
-        pos_embedding_ctxt = apply_masks_over_embedded_patches(pos_embedding_ctxt, masks_ctxt)
-        pos_embedding_pred_target = apply_masks_over_embedded_patches(pos_embedding_pred_target, masks_pred_target)  
+        # #apply the masks on the appropriate positional embedding. 
+        # pos_embedding_ctxt = apply_masks_over_embedded_patches(pos_embedding_ctxt, masks_ctxt)
+        # pos_embedding_pred_target = apply_masks_over_embedded_patches(pos_embedding_pred_target, masks_pred_target)  
 
         #we basically took the image size embedding, mask it to the context and added it to the context-masked image embedding that came from the encoder.
         x += pos_embedding_ctxt
         
-        #we will be adding the pred tokens to the positional embedding that's been masked by the pred/target masks.
-        pred_tokens = self.mask_token.repeat(pos_embedding_pred_target.size(0), pos_embedding_pred_target.size(1), 1) 
-        pred_tokens += pos_embedding_pred_target
+        # #we will be adding the pred tokens to the positional embedding that's been masked by the pred/target masks.
+        # pred_tokens = self.mask_token.repeat(pos_embedding_pred_target.size(0), pos_embedding_pred_target.size(1), 1) 
+        # pred_tokens += pos_embedding_pred_target
 
-        #next, the pred tokens has to be concatenated to the x. The tokens are to be concatenated in the first dimension. REMEBER, in the first dimension of 'x', where the patch index should be, all the pred/target masks indices shouldn't be present since the context mask is a complement of them. The idea here is to concat the indices to 'x' and eventually, the network will learn to predict the embeddings in those pred/target indices. 
-        x = x.repeat(len(masks_pred_target), 1, 1) #we need to repeat the batch dimension first in order to concat with the pred_tokens.
-        x = torch.cat([x, pred_tokens], dim=1)
+        # #next, the pred tokens has to be concatenated to the x. The tokens are to be concatenated in the first dimension. REMEBER, in the first dimension of 'x', where the patch index should be, all the pred/target masks indices shouldn't be present since the context mask is a complement of them. The idea here is to concat the indices to 'x' and eventually, the network will learn to predict the embeddings in those pred/target indices. 
+        # x = x.repeat(len(masks_pred_target), 1, 1) #we need to repeat the batch dimension first in order to concat with the pred_tokens.
+        # x = torch.cat([x, pred_tokens], dim=1)
 
         #run through the predictor network.
         x = self.transformer_blocks(x)
         x = self.final_layernorm(x)
 
         #after the input goes through the transformer network, we want to return the predictions to perform target loss. However, we do not need the context mask. Since we concatenated everything along the way, the first 'num_ctxt' value in the first dimension, can be ignored. 
-        x = x[:, num_ctxt:]
-        x = self.predictor_projector(x)
+        # x = x[:, num_ctxt:]
+        x = self.classification_head(x)
 
         return x
 
@@ -114,13 +120,15 @@ class VisionTransformerForEncoder(nn.Module):
                                                           **kwargs
                                                           ).to(device) 
 
-        #apply layernorm on the output of the transformer blocks.
-        self.classification_head = nn.Sequential(einops_torch.Reduce('b n e -> b e', reduction='mean'),
-                                                 nn.LayerNorm(self.encoder_network_embedding_dim),
-                                                 nn.Linear(self.encoder_network_embedding_dim, self.encoder_network_embedding_dim*2),
-                                                 nn.GELU(),
-                                                 nn.Linear(self.encoder_network_embedding_dim*2, num_classes))
+        
+        # self.classification_head = nn.Sequential(einops_torch.Reduce('b n e -> b e', reduction='mean'),
+        #                                          nn.LayerNorm(self.encoder_network_embedding_dim),
+        #                                          nn.Linear(self.encoder_network_embedding_dim, self.encoder_network_embedding_dim*2),
+        #                                          nn.GELU(),
+        #                                          nn.Linear(self.encoder_network_embedding_dim*2, num_classes))
 
+        #apply layernorm on the output of the transformer blocks.
+        self.final_layernorm = nn.LayerNorm(self.encoder_network_embedding_dim).to(device)
 
         
     
@@ -136,14 +144,16 @@ class VisionTransformerForEncoder(nn.Module):
         pos_embed_module = PositionalEncoder(token_length=x.size(1), output_dim=x.size(2), n=10000, device=self.device)
         pos_embedding_tensor = pos_embed_module()
         
+
+        stacked_pos_enc_tensor = einops.repeat(pos_embedding_tensor.unsqueeze(0), '() p e -> b p e', b=x.size(0)).detach()
         #concat the pos embedding tensor the patch embedding.
-        x = x + pos_embedding_tensor
+        x = x + stacked_pos_enc_tensor
 
         if masks is not None:
             x = apply_masks_over_embedded_patches(x, masks)
 
         x = self.transformer_blocks(x)
-        x = self.classification_head(x)
+        x = self.final_layernorm(x)
 
         return x
         
